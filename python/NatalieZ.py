f10aa11e80d2aeffeb73b4ee8cc698d6ee041c35
Natalia Zhur <namelessgirla@gmail.com>
2024-11-05 16:59:37 -0600

    python session Nov 1

--- python/NatalieZ.py
+++ python/NatalieZ.py
@@ -1,3 +1,5 @@
+from operator import truediv
+
 print("Hello World!")
 first_name = "Natalia"
 last_name = "Z"
@@ -35,6 +37,8 @@ if answer_3 == "/":
 
 answer = input("choose \n1 to convert foot -> meter \n2 to convert meter -> foot: ")
 
+#Convert foot to meter
+
 if answer == "1":
     foot = float(input("enter foot: "))
     meter = foot / 3.281
@@ -45,3 +49,56 @@ elif answer == "2":
     print(foot)
 else:
     print("wrong input please try again")
+
+# Convert C to F
+
+answer = input("Choose \n1 to convert celsius -> fahrenheit, n2 to convert fahrenheit -> celsius: ")
+if answer == "1":
+    celsius = float(input("Enter Celsius: "))
+    fahrenheit = (celsius * 1.8) + 32
+    print(fahrenheit)
+elif answer == "2":
+    fahrenheit = float(input("Enter fahrenheit: "))
+    celsius = (fahrenheit - 32)*1.8
+    print(celsius)
+else:
+    print("Wrong input, please type 1 or 2")
+
+
+answer = input("Enter 1 to convert feet to meters or 2 to convert meters to feet: ")
+
+if answer == "1":
+    foot = float(input("Enter foot (0-1000): "))
+    if foot < 0 or foot > 1000:
+        print("Value out of range")
+    else:
+        meter = foot / 3.281
+        print(f"{foot} feet is equal to {meter:.2f} meters")
+elif answer == "2":
+    meter = float(input("Enter meter (0-1000): "))
+    if meter < 0 or meter > 1000:
+        print("Value out of range")
+    else:
+        foot = meter * 3.281
+        print(f"{meter} meters is equal to {foot:.2f} feet")
+else:
+    print("Wrong input, please try again")
+
+a: bool = True
+b: bool = False
+
+#AND
+True True = True
+False True = False
+True False = False
+False False = False
+
+# OR
+True True = True
+False True = True
+True False = True
+False False = False
+
+#NOT
+not True = False
+not False = True
