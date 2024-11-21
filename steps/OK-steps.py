52ef45e387fb7ecdb4e37558111185fcb2432da7
Olga K. <olga_kisilyova@yahoo.com>
2024-11-21 09:26:22 -0700

    Step for Clicking menu items, API tests

--- steps/OK-steps.py
+++ steps/OK-steps.py
@@ -77,14 +77,14 @@ def ok_click_button(context, name):
         'Update': "//button/span[text()='Update']",
         'Send': "//button[text() = ' Send ']",
         'Device settings': "//a[@href='#/device-settings']/i",
-        'Device1': "(//button[text() = ' Edit '])[1]" # second device on the page for testing the rename feature
+        'Device1': "(//button[text() = ' Edit '])[1]" #  second device on the page for testing the rename feature
     }
     element = WebDriverWait(context.driver, 10).until(EC.element_to_be_clickable((By.XPATH, buttons[name])))
     # element = context.driver.find_element(By.XPATH, buttons[name])
     element.click()
 
 
-@step('OK Create new record for device with following data') # API
+@step('OK Create new record for device with following data') #  API
 def ok_add_new_device(context):
     endpoint = 'device-data/airguard/log/v1'
     data = {}
@@ -92,14 +92,15 @@ def ok_add_new_device(context):
         data[row.cells[0]] = row.cells[1]
 
     message = (
-        f"{data['imei']},{data['date']}191400.000,{data['jump']},3249.34,676.98,32.22,-115,1,1,{data['date']}211429.000,"
+        f"{data['imei']},{data['date']}191400.000,{data['jump']},3249.34,676.98,32.22,-115,1,1,"
+        f"{data['date']}211429.000,"
         f"{data['latitude']},{data['longitude']},2573.600,,,1,,2.5,,,,20,4,,,28,,,|,771,-115,1723324527")
 
     response = requests.post(BASE_URL + endpoint, data=message)
     assert response.status_code == 201, f"{response.status_code}: {response.text}"
 
 
-@step('OK Create new heartbeat message for device with following data') # API
+@step('OK Create new heartbeat message for device with following data') #  API
 def ok_add_new_device1(context):
     endpoint = 'device-data/airguard/heartbeat/v1'
     data = {}
